25c252b2372da6876a53041918946c46c057fc5d
Heath Boutilier <heathboutilier@outlook.com>
2025-07-07 20:21:54 -0700

    c2l3

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -16,6 +20,7 @@ def main():
         screen.fill("black")
 
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
